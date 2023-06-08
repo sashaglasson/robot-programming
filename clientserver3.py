5ede6e764a000b51954307935993c57ba7369eef
sashaglasson <54581793+sashaglasson@users.noreply.github.com>
2023-06-08 13:22:12 +0100

    Choregraphe code

---
+++ clientserver3.py
@@ -0,0 +1,31 @@
+import socket
+import time
+
+class ClientServer2:
+    clientsocket = None
+    address = None
+    listensocket = socket.socket()  # Creates an instance of socket
+    Port = 7759  # Port to host server on
+    maxConnections = 999
+    IP = "192.168.0.102"  # IP address of local machine
+    
+    @staticmethod
+    def start_server():
+        ClientServer2.listensocket.bind(('', ClientServer2.Port))
+    
+        # Starts server
+        
+        ClientServer2.listensocket.listen(ClientServer2.maxConnections)
+        print("Server started at " + ClientServer2.IP + " on port " + str(ClientServer2.Port))
+
+    # Accepts the incoming connection
+    # one for each client
+    
+    @staticmethod
+    def accept_connection():
+        ClientServer2.clientsocket, ClientServer2.address = ClientServer2.listensocket.accept()
+
+    @staticmethod
+    def listen_for_messages():
+        message = ClientServer2.clientsocket.recv(1024).decode()  # Gets the incoming message
+        return message
---
+++ test.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="test" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="server_test" src="clientserver.py" />
+        <File name="clientserver2" src="clientserver2.py" />
+        <File name="clientserver3" src="clientserver3.py" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
